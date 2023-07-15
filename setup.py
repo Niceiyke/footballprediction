ed6b9b7b6290fba7aea29b282693f3f8316093fe
Niceiyke <oyomworld@gmail.com>
2023-07-15 16:06:50 +0100

    update

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ version='0.0.1',
 author='niceiyke',
 author_email='niceiyke04@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+install_requires=['pandas','numpy','scikit-learn']
 
 )
